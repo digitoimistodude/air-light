5a237493162c6cdb3222c890749321343d3ba12f
Roni Laukkarinen <roni@laukkarinen.info>
2016-02-24 10:52:50 +0200

    Fix content-none container

--- 404.php
+++ 404.php
@@ -20,41 +20,6 @@ get_header(); ?>
 
 					<div class="page-content">
 						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'air' ); ?></p>
-
-						<?php
-							get_search_form();
-
-							the_widget( 'WP_Widget_Recent_Posts' );
-
-							// Only show the widget if site has multiple categories.
-							if ( air_categorized_blog() ) :
-						?>
-
-						<div class="widget widget_categories">
-							<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'air' ); ?></h2>
-							<ul>
-							<?php
-								wp_list_categories( array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								) );
-							?>
-							</ul>
-						</div><!-- .widget -->
-
-						<?php
-							endif;
-
-							/* translators: %1$s: smiley */
-							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'air' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-							the_widget( 'WP_Widget_Tag_Cloud' );
-						?>
-
 					</div><!-- .page-content -->
 				</section><!-- .error-404 -->
 
--- index.php
+++ index.php
@@ -14,39 +14,36 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		if ( have_posts() ) : ?>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
 
 		<div class="container">
 
+			<?php
+			if ( have_posts() ) : ?>
+
 			<?php if ( is_home() && ! is_front_page() ) : ?>
+
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 
-			<?php
-			endif;
+			<?php endif; ?>
 
-			while ( have_posts() ) : the_post(); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+				<?php endwhile; the_posts_navigation(); ?>
 
-				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-
-			<?php endwhile;
-			the_posts_navigation();
-
-			else : ?>
+			<?php else : ?>
 
 				<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
+			<?php endif; ?>
+
 		</div><!-- .container -->
 
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
