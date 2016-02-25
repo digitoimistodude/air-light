543633ae3dd6888c21d8cf2198de637e215790aa
Roni Laukkarinen <roni@laukkarinen.info>
2016-02-25 14:26:08 +0200

    Add accessible CSS animations to nav, remove jquery animate (thanks @samikeijonen)

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
  /**
   * The current version of the theme.
   */
- define( 'AIR_VERSION', '1.1.6' );
+ define( 'AIR_VERSION', '1.1.7' );
 
  /*
   * Allow Gravity Forms to hide labels to add placeholders
