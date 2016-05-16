9b77ea6b733ab75dcf870a8acc0749684783cc99
Roni Laukkarinen <roni@laukkarinen.info>
2016-05-16 23:46:18 +0300

    Add short description for front page

--- front-page.php
+++ front-page.php
@@ -1,5 +1,12 @@
 <?php
 /**
+ * The template for displaying front page.
+ *
+ * Contains the closing of the #content div and all content after.
+ * Initial styles for front page template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
  * @package air
  */
 
