4338fa5e2441788c663e94c4191343a3cd5e06f1
Roni Laukkarinen <roni@laukkarinen.info>
2016-05-16 21:53:54 +0300

    Escape wp_get_attachment_url

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -7,8 +7,9 @@
  *
  * @package air
  */
+
 ?>
 
-<div class="slide" style="background-image:url('<?php if ( has_post_thumbnail() ) : ?><?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?><?php else : ?><?php echo get_template_directory_uri(); ?>/images/slide.jpg<?php endif; ?>');">
+<div class="slide" style="background-image:url('<?php if ( has_post_thumbnail() ) : ?><?php echo esc_url(wp_get_attachment_url( get_post_thumbnail_id($post->ID) )); ?><?php else : ?><?php echo get_template_directory_uri(); ?>/images/slide.jpg<?php endif; ?>');">
   <div class="shade"></div>
 </div>
