2a12339ebdb4519e07978fc6ffe82da936c98fc0
Roni Laukkarinen <roni@laukkarinen.info>
2016-05-23 12:27:36 +0300

    Add non-responsive font stack for quick use cases

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
  /**
   * The current version of the theme.
   */
- define( 'AIR_VERSION', '1.4.5' );
+ define( 'AIR_VERSION', '1.4.6' );
 
  /**
   * Allow Gravity Forms to hide labels to add placeholders
