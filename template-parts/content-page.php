5ff67d1188fa69441b18639a0af26d6d50a07169
Roni Laukkarinen <roni@laukkarinen.info>
2016-06-28 17:36:42 +0300

    Indent  in
    Ref: https://github.com/Automattic/_s/commit/ce8d6cc3bab748d6193449853d9cf1ac62128b3a

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -30,20 +30,20 @@
     </div><!-- .entry-content -->
 
     <?php if ( get_edit_post_link() ) : ?>
-    <footer class="entry-footer">
-      <?php
-        edit_post_link(
-          sprintf(
-            /* translators: %s: Name of current post */
-            esc_html__( 'Edit %s', 'air' ),
-            the_title( '<span class="screen-reader-text">"', '"</span>', false )
-          ),
-          '<p class="edit-link">',
-          '</p>'
-        );
-      ?>
-    </footer><!-- .entry-footer -->
-    <?php endif; ?>
+  		<footer class="entry-footer">
+  			<?php
+  				edit_post_link(
+  					sprintf(
+  						/* translators: %s: Name of current post */
+  						esc_html__( 'Edit %s', '_s' ),
+  						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+  					),
+  					'<span class="edit-link">',
+  					'</span>'
+  				);
+  			?>
+  		</footer><!-- .entry-footer -->
+  	<?php endif; ?>
   </article><!-- #post-## -->
 
 </div><!-- .container -->
