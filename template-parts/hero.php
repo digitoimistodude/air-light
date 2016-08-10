7e1b7f774bd94c93dc44a8df8e7ec2801ab83897
Roni Laukkarinen <roni@laukkarinen.info>
2016-08-10 11:46:31 +0300

    Change default hero image name to more consistent

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -10,6 +10,6 @@
 
 ?>
 
-<div class="slide" style="background-image:url('<?php if ( has_post_thumbnail() ) : ?><?php echo esc_url(wp_get_attachment_url( get_post_thumbnail_id($post->ID) ) ); ?><?php else : ?><?php echo esc_url(get_template_directory_uri(). '/images/slide.jpg'); ?><?php endif; ?>');">
+<div class="slide" style="background-image:url('<?php if ( has_post_thumbnail() ) : ?><?php echo esc_url(wp_get_attachment_url( get_post_thumbnail_id($post->ID) ) ); ?><?php else : ?><?php echo esc_url(get_template_directory_uri(). '/images/default.jpg'); ?><?php endif; ?>');">
   <div class="shade"></div>
 </div>
