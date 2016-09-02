48a1de941e395c4c11ae8ad59aa4cd711c304393
Roni Laukkarinen <roni@laukkarinen.info>
2016-09-02 12:08:00 +0300

    Bump version to 1.6.7

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 /**
  * The current version of the theme.
  */
-define( 'AIR_VERSION', '1.6.5' );
+define( 'AIR_VERSION', '1.6.7' );
 
 /**
  * Allow Gravity Forms to hide labels to add placeholders
