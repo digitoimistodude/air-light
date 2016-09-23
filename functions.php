00be8e8e8e48ee45d342f4acb7982a94e18bfe21
Roni Laukkarinen <roni@laukkarinen.info>
2016-09-23 15:33:58 +0300

    Update release to 1.7.2, implement _s changes

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 /**
  * The current version of the theme.
  */
-define( 'AIR_VERSION', '1.7.1' );
+define( 'AIR_VERSION', '1.7.2' );
 
 /**
  * Allow Gravity Forms to hide labels to add placeholders
