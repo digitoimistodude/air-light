1801e509bebbb80c012b7aaae465208a0bd844df
Roni Laukkarinen <roni@laukkarinen.info>
2016-11-01 13:08:38 +0200

    Gallery fixes, add columns based on wp-admin option

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 /**
  * The current version of the theme.
  */
-define( 'AIR_VERSION', '1.7.4' );
+define( 'AIR_VERSION', '1.8.0' );
 
 /**
  * Allow Gravity Forms to hide labels to add placeholders
