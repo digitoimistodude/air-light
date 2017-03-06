68a231ab3d9a52c159bd3451271ef422fac20fb0
Roni Laukkarinen <roni@laukkarinen.info>
2017-03-06 16:31:17 +0200

    Remove useless if check on front page

--- front-page.php
+++ front-page.php
@@ -12,7 +12,7 @@
 
 get_header(); ?>
 
-<div class="slide<?php if ( is_front_page() ) : echo ' slide-front'; elseif ( is_page() ) : echo ' slide-page'; else : echo ' slide-' . get_post_type(); endif; ?>" style="background-image:url('<?php if ( has_post_thumbnail() ) : ?><?php echo esc_url(wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ) ); ?><?php else : ?><?php echo esc_url( get_template_directory_uri() . '/images/default.jpg' ); ?><?php endif; ?>');">
+<div class="slide slide-front" style="background-image:url('<?php if ( has_post_thumbnail() ) : ?><?php echo esc_url(wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ) ); ?><?php else : ?><?php echo esc_url( get_template_directory_uri() . '/images/default.jpg' ); ?><?php endif; ?>');">
   <div class="shade shade-gradient"></div>
 
   <div class="container">
