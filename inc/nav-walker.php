613dfc4dd7ebec20c3d138a13c9665c1c43087e8
Roni Laukkarinen <roni@dude.fi>
2017-07-05 16:25:50 +0300

    Fix nav walker title -> attr_title; show title attr only when defined in WP

--- inc/nav-walker.php
+++ inc/nav-walker.php
@@ -60,7 +60,7 @@ class Air_Walker extends Walker_Nav_Menu {
 			$output .= $indent . '<li' . $id . $value . $class_names . '>';
 
 			$atts = array();
-			$atts['title']  = ! empty( $item->title )	? $item->title	: '';
+			$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
 			$atts['target'] = ! empty( $item->target )	? $item->target	: '';
 			$atts['rel']    = ! empty( $item->xfn )		? $item->xfn	: '';
 
