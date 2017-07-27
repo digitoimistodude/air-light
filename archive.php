f2449361a0b5b1245867ac5485eedbc6329de354
Roni Laukkarinen <roni@laukkarinen.info>
2017-07-27 14:48:44 +0300

    Fix travis errors for PHP 5.6

--- archive.php
+++ archive.php
@@ -18,20 +18,24 @@ get_template_part( 'template-parts/hero', get_post_type() ); ?>
       <?php if ( have_posts() ) : ?>
 
         <header class="page-header">
-          <?php the_archive_title( '<h1 class="page-title">', '</h1>' );
-          the_archive_description( '<div class="taxonomy-description">', '</div>' ); ?>
+          <?php
+            the_archive_title( '<h1 class="page-title">', '</h1>' );
+            the_archive_description( '<div class="taxonomy-description">', '</div>' );
+          ?>
         </header><!-- .page-header -->
 
-        <?php while ( have_posts() ) {
-        	the_post();
-        	get_template_part( 'template-parts/content', get_post_type() );
+        <?php
+          while ( have_posts() ) {
+        	 the_post();
+        	 get_template_part( 'template-parts/content', get_post_type() );
         }
 
         the_posts_navigation();
 
       else :
       	get_template_part( 'template-parts/content', 'none' );
-      endif; ?>
+      endif;
+      ?>
 
     </div><!-- .container -->
 
