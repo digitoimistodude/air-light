a25bf3adfa32259204c8e97e1096cb7a53ab4494
Roni Laukkarinen <roni@laukkarinen.info>
2017-09-05 14:14:52 +0300

    Improve "Continue reading" word order for i18n
    https://github.com/Automattic/_s/commit/ef2a0e7c00bfa573f6835c12c3917bb268fb4593

--- template-parts/content.php
+++ template-parts/content.php
@@ -28,16 +28,23 @@
 
     <div class="entry-content">
       <?php
-        the_content( sprintf(
-          /* translators: %s: Name of current post. */
-          wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'air' ), array( 'span' => array( 'class' => array() ) ) ),
-          the_title( '<span class="screen-reader-text">"', '"</span>', false )
-        ) );
+      the_content( sprintf(
+        wp_kses(
+          /* translators: %s: Name of current post. Only visible to screen readers */
+          __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', '_s' ),
+          array(
+            'span' => array(
+              'class' => array(),
+            ),
+          )
+        ),
+        get_the_title()
+      ) );
 
-        wp_link_pages( array(
-          'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'air' ),
-          'after'  => '</div>',
-        ) );
+      wp_link_pages( array(
+        'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'air' ),
+        'after'  => '</div>',
+      ) );
       ?>
     </div><!-- .entry-content -->
 
