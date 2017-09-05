53039be905bf72d3b58faf7285a60cb04259dd99
Roni Laukkarinen <roni@laukkarinen.info>
2017-09-05 14:26:02 +0300

    Proper indentation
    Ref: https://github.com/Automattic/_s/commit/32a9dbd5d5557b7614f3bf6e2ea059adbcc925a2

--- header.php
+++ header.php
@@ -9,15 +9,16 @@
  * @package air
  */
 
-?><!doctype html>
+?>
+<!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/favicon.png">
-
-<?php wp_head(); ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/favicon.png">
+  
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
