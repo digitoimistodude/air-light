8278d7fc54014280a31eaad2898a9f29b6d87aba
Roni Laukkarinen <roni@laukkarinen.info>
2018-01-30 11:46:51 +0200

    Fix textdomains

--- functions.php
+++ functions.php
@@ -43,9 +43,9 @@ if ( ! isset( $content_width ) ) {
  */
 function _air_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', '_s' ),
+		'name'          => esc_html__( 'Sidebar', 'air' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', '_s' ),
+		'description'   => esc_html__( 'Add widgets here.', 'air' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
