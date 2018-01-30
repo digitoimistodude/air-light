1a3bf3b1380d75288d82b7c5eaab38fa176136b4
Roni Laukkarinen <roni@laukkarinen.info>
2018-01-30 11:37:25 +0200

    Add default widget area as recommended in best practices

--- sidebar.php
+++ sidebar.php
@@ -7,8 +7,11 @@
  *
  * @package air
  */
-
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+  return;
+}
 ?>
 
 <aside id="secondary" class="widget-area">
+  <?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
