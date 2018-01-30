d4f1be4910e332f4e278ce65de8395a898d0135c
Roni Laukkarinen <roni@laukkarinen.info>
2018-01-30 11:46:13 +0200

    register_sidebar from _s

--- front-page.php
+++ front-page.php
@@ -35,6 +35,8 @@ get_header(); ?>
 
       <div class="container">
 
+        <?php the_post_thumbnail(); ?>
+
         <?php if ( have_posts() ) {
         	while ( have_posts() ) {
 	      		the_post();
--- functions.php
+++ functions.php
@@ -37,6 +37,24 @@ if ( ! isset( $content_width ) ) {
 }
 
 /**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function _air_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', '_s' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', '_s' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+}
+add_action( 'widgets_init', '_air_widgets_init' );
+
+/**
  * Enqueue scripts and styles.
  */
 function air_scripts() {
