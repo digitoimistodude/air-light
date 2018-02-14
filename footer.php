a2f2b18958fdedbafb8ec4778a55a4f3c86f4941
Roni Laukkarinen <roni@laukkarinen.info>
2018-02-14 14:44:16 +0200

    Lint footer

--- footer.php
+++ footer.php
@@ -34,7 +34,7 @@
 
 <?php wp_footer(); ?>
 
-<a href="#page" class="js-trigger top" data-mt-duration="300"><span class="screen-reader-text"><?php echo esc_html_e('Back to top', 'air'); ?></span><?php include get_theme_file_path( '/svg/chevron-up.svg' ); ?></a>
+<a href="#page" class="js-trigger top" data-mt-duration="300"><span class="screen-reader-text"><?php echo esc_html_e( 'Back to top', 'air' ); ?></span><?php include get_theme_file_path( '/svg/chevron-up.svg' ); ?></a>
 
 </body>
 </html>
