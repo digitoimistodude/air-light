ea750651cca9931e91aa948a7ca2c597fe2855a4
Roni Laukkarinen <roni@laukkarinen.info>
2018-03-26 10:32:24 +0300

    wp_footer() needs to be immediately before </body> according to standards

--- footer.php
+++ footer.php
@@ -32,9 +32,8 @@
 
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
-
 <a href="#page" class="js-trigger top" data-mt-duration="300"><span class="screen-reader-text"><?php echo esc_html_e( 'Back to top', 'air-light' ); ?></span><?php include get_theme_file_path( '/svg/chevron-up.svg' ); ?></a>
 
+<?php wp_footer(); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @package air-light
  */
 
-define( 'AIR_VERSION', '3.7.9' );
+define( 'AIR_VERSION', '3.8.0' );
 
 /**
  * Requires.
