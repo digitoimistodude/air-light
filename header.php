22d9e5cfedc92692f4d50bafa54e669f3dd2efff
Roni Laukkarinen <roni@laukkarinen.info>
2018-05-08 12:04:29 +0300

    Indentation rules

--- header.php
+++ header.php
@@ -21,61 +21,61 @@
 </head>
 
 <body <?php body_class( 'no-js' );; ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'air-light' ); ?></a>
+  <div id="page" class="site">
+   <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'air-light' ); ?></a>
 
-  <div class="nav-container">
-      <header class="site-header">
+   <div class="nav-container">
+    <header class="site-header">
 
-          <div class="site-branding">
-            <?php if ( is_front_page() && is_home() ) : ?>
-              <h1 class="site-title">
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                  <span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
-                  <?php include get_theme_file_path( '/svg/logo.svg' ); ?>
-                </a>
-              </h1>
-            <?php else : ?>
-              <p class="site-title">
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                  <span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
-                  <?php include get_theme_file_path( '/svg/logo.svg' ); ?>
-                </a>
-              </p>
-            <?php endif;
+      <div class="site-branding">
+        <?php if ( is_front_page() && is_home() ) : ?>
+        <h1 class="site-title">
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
+            <?php include get_theme_file_path( '/svg/logo.svg' ); ?>
+          </a>
+        </h1>
+        <?php else : ?>
+          <p class="site-title">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+              <span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
+              <?php include get_theme_file_path( '/svg/logo.svg' ); ?>
+            </a>
+          </p>
+        <?php endif;
 
-            $description = get_bloginfo( 'description', 'display' );
-            if ( $description || is_customize_preview() ) : ?>
-              <p class="site-description screen-reader-text"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-            <?php endif; ?>
-          </div><!-- .site-branding -->
+        $description = get_bloginfo( 'description', 'display' );
+        if ( $description || is_customize_preview() ) : ?>
+          <p class="site-description screen-reader-text"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+        <?php endif; ?>
+      </div><!-- .site-branding -->
 
-          <div class="main-navigation-wrapper" id="main-navigation-wrapper">
+      <div class="main-navigation-wrapper" id="main-navigation-wrapper">
 
-            <button id="nav-toggle" class="nav-toggle hamburger" type="button" aria-label="<?php esc_attr_e( 'Menu', 'air-light' ); ?>" aria-controls="navigation">
-              <span class="hamburger-box">
-                <span class="hamburger-inner"></span>
-              </span>
-              <span id="nav-toggle-label" class="screen-reader-text" aria-label="<?php esc_attr_e( 'Menu', 'air-light' ); ?>"><?php esc_attr_e( 'Menu', 'air-light' ); ?></span>
-            </button>
+        <button id="nav-toggle" class="nav-toggle hamburger" type="button" aria-label="<?php esc_attr_e( 'Menu', 'air-light' ); ?>" aria-controls="navigation">
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+          <span id="nav-toggle-label" class="screen-reader-text" aria-label="<?php esc_attr_e( 'Menu', 'air-light' ); ?>"><?php esc_attr_e( 'Menu', 'air-light' ); ?></span>
+        </button>
 
-            <nav id="nav" class="nav-primary" role="navigation">
+        <nav id="nav" class="nav-primary" role="navigation">
 
-              <?php wp_nav_menu( array(
-                'theme_location'    => 'primary',
-                'container'         => false,
-                'depth'             => 4,
-                'menu_class'        => 'menu-items',
-                'menu_id'           => 'main-menu',
-                'echo'              => true,
-                'fallback_cb'       => 'Air_Light_Navwalker::fallback',
-                'items_wrap'        => '<ul class="%2$s" id="%1$s">%3$s</ul>',
-                'walker'            => new Air_Light_Navwalker(),
-              ) ); ?>
+          <?php wp_nav_menu( array(
+            'theme_location'    => 'primary',
+            'container'         => false,
+            'depth'             => 4,
+            'menu_class'        => 'menu-items',
+            'menu_id'           => 'main-menu',
+            'echo'              => true,
+            'fallback_cb'       => 'Air_Light_Navwalker::fallback',
+            'items_wrap'        => '<ul class="%2$s" id="%1$s">%3$s</ul>',
+            'walker'            => new Air_Light_Navwalker(),
+          ) ); ?>
 
-            </nav><!-- #nav -->
-          </div>
-      </header>
+        </nav><!-- #nav -->
+      </div>
+    </header>
   </div><!-- .nav-container -->
 
-	<div class="site-content">
+  <div class="site-content">
--- template-parts/content.php
+++ template-parts/content.php
@@ -12,13 +12,14 @@
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header">
       <?php
-        if ( is_single() ) {
-          the_title( '<h1 class="entry-title">', '</h1>' );
-        } else {
-          the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-        }
+      if ( is_single() ) {
+        the_title( '<h1 class="entry-title">', '</h1>' );
+      } else {
+        the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+      }
 
-      if ( 'post' === get_post_type() ) : ?>
+      if ( 'post' === get_post_type() ) :
+      ?>
       <div class="entry-meta">
         <p class="entry-time">
           <time datetime="<?php the_time( 'c' ); ?>"><?php echo get_the_date( get_option( 'date_format' ) ); ?></time>
