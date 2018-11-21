198843c26299abb991d2c2e35ef85eacc0fbd0b0
Roni Laukkarinen <roni@laukkarinen.info>
2018-11-21 13:35:23 +0200

    Install a11y validator

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,27 @@ REQUIRED STUFF
 ==============
 */
 
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var sourcemaps   = require('gulp-sourcemaps');
-var browsersync  = require('browser-sync').create();
-var notify       = require('gulp-notify');
-var prefix       = require('gulp-autoprefixer');
-var cleancss     = require('gulp-clean-css');
-var uglify       = require('gulp-uglify-es').default;
-var concat       = require('gulp-concat');
-var util         = require('gulp-util');
-var header       = require('gulp-header');
-var pixrem       = require('gulp-pixrem');
-var exec         = require('child_process').exec;
-var rename       = require('gulp-rename');
-var stylefmt     = require('gulp-stylefmt');
-var debug        = require('gulp-debug');
-var scsslint     = require('gulp-scss-lint');
-var cache        = require('gulp-cached');
-var phpcs        = require('gulp-phpcs');
-var validatehtml = require('gulp-w3c-html-validation');
+var gulp           = require('gulp');
+var sass           = require('gulp-sass');
+var sourcemaps     = require('gulp-sourcemaps');
+var browsersync    = require('browser-sync').create();
+var notify         = require('gulp-notify');
+var prefix         = require('gulp-autoprefixer');
+var cleancss       = require('gulp-clean-css');
+var uglify         = require('gulp-uglify-es').default;
+var concat         = require('gulp-concat');
+var util           = require('gulp-util');
+var header         = require('gulp-header');
+var pixrem         = require('gulp-pixrem');
+var exec           = require('child_process').exec;
+var rename         = require('gulp-rename');
+var stylefmt       = require('gulp-stylefmt');
+var debug          = require('gulp-debug');
+var scsslint       = require('gulp-scss-lint');
+var cache          = require('gulp-cached');
+var phpcs          = require('gulp-phpcs');
+var validatehtml   = require('gulp-w3c-html-validation');
+var a11y           = require('gulp-accessibility');
 
 /*
 
@@ -49,7 +50,7 @@ var handleError = function(task) {
   return function(err) {
 
       notify.onError({
-        message: task + ' failed, check the logs..'
+        message: task + ' failed, check the logs...'
       })(err);
 
     util.log(util.colors.bgRed(task + ' error:'), util.colors.red(err));
@@ -210,6 +211,7 @@ VALIDATE HTML
 =============
 */
 
+// Validator for: https://validator.w3.org/
 gulp.task('validatehtml', function() {
   return gulp.src([phpSrc, '!functions.php', '!node_modules/**/*', '!inc/**/*'])
     .pipe(validatehtml({
@@ -218,6 +220,8 @@ gulp.task('validatehtml', function() {
         errorTemplate: null,
         reportpath: false,
         doctype: 'HTML5',
+
+        // Ignore WordPress/PHP-related/file structure related error messages
         relaxerror: [/XML processing/g,
         /role is unnecessary for element/g,
         /Text not allowed in element “ol” in this context/g,
@@ -232,6 +236,12 @@ gulp.task('validatehtml', function() {
         /Attribute “post_/g,
         /An ID must not contain whitespace/g,
         /Attribute “\?” not allowed on element/g,
+        /Attribute “{” not allowed on element/g,
+        /“echo”/g,
+        /“%1\$s”/g,
+        /Attribute “'id” not allowed on element/g,
+        /Attribute “';” not allowed on element/g,
+        /Attribute “}” not allowed on element/g,
         /Attribute “\$/g,
         /Bad value “%s”/g,
         /Bad value “<\?php/g,
@@ -246,18 +256,57 @@ gulp.task('validatehtml', function() {
         /Start tag seen without seeing a doctype first/g,
         /Illegal character in path segment/g,
         /is not serializable as XML/g,
+        /No space between attributes./g,
+        /Saw “'” when/g,
         /End tag seen without seeing a doctype first/g,
         /Non-space characters found without seeing a doctype first/g,
         /End of file seen without seeing a doctype first/g,
         /Consider adding a “lang” attribute to the “html”/g,
+        /Matching quote missing/g,
         /"End tag for  “body” seen/g,
         /The character encoding was not declared/g,
         /Empty heading./g,
         /Cannot recover after last error/g,
+        /Bad value “mailto: <\?php/g,
+        /Bad value “tel: <\?/g,
+        /Bad value “mailto:<\?php/g,
+        /Bad value “tel:<\?/g,
+        /<\?php/g,
+        /This document appears to be written/g,
         /The document is not mappable to XML/g]
     }))
 });
 
+/*
+
+ACCESSIBILITY
+=============
+*/
+
+gulp.task('a11y', function() {
+  return gulp.src([phpSrc, '!functions.php', '!node_modules/**/*', '!inc/**/*'])
+    .pipe(a11y({
+      accessibilityLevel: 'WCAG2A',
+      verbose: true,
+      force: true,
+      reportLevels: {
+        notice: false,
+        warning: true,
+        error: true
+      },
+
+      // Ignore WordPress/PHP-related/file structure related error messages
+      ignore: [
+        'WCAG2A.Principle3.Guideline3_1.3_1_1.H57.2',
+        'WCAG2A.Principle2.Guideline2_4.2_4_2.H25.1.NoTitleEl',
+        'WCAG2A.Principle4.Guideline4_1.4_1_2.H91.A.NoContent',
+        'WCAG2A.Principle1.Guideline1_3.1_3_1.H42.2',
+        'WCAG2A.Principle2.Guideline2_4.2_4_1.G1,G123,G124.NoSuchID'
+      ]
+    }))
+    .on('error', console.log)
+});
+d
 /*
 
 SCRIPTS
@@ -298,7 +347,7 @@ gulp.task('js-watch', ['js'], browsersync.reload);
 gulp.task('watch', ['browsersync'], function() {
 
   gulp.watch(sassSrc, ['styles', 'scss-lint']).on( 'change', helpers );
-  gulp.watch(phpSrc, ['phpcs', 'validatehtml']);
+  gulp.watch(phpSrc, ['phpcs', 'validatehtml', 'a11y']);
   gulp.watch(jsSrc, ['js-watch']);
 
 });
