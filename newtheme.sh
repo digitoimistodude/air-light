13085099f7071a9b404c094db7a8804a927ecaba
Roni Laukkarinen <roni@laukkarinen.info>
2019-01-28 11:35:37 +0200

    Update ignored_names, add default README template to theme generator script

--- newtheme.sh
+++ newtheme.sh
@@ -82,6 +82,72 @@ echo "${yellow}Adding media library folder...${txtreset}"
 mkdir -p ${PROJECTPATH}/media
 echo "" > ${PROJECTPATH}/media/index.php
 chmod 777 ${PROJECTPATH}/media
+
+echo "${yellow}Generating default README.md...${txtreset}"
+
+newestair="4.5.8"
+newestwordpress="5.0.3"
+newestphp="7.2"
+currentdate="LC_TIME=en_US date '+%d %b %Y'";
+datewithunderscores="${currentdate} |tr ' ' '_'"
+echo "# ${PROJECTNAME}
+![based_on_air_version ${newestair}_](https://img.shields.io/badge/based_on_air_version-${newestair}_-brightgreen.svg?style=flat-square) ![project_created ${datewithunderscores}](https://img.shields.io/badge/project_created-${datewithunderscores}-blue.svg?style=flat-square) ![Tested_up_to WordPress_${newestwordpress}](https://img.shields.io/badge/Tested_up_to-WordPress_${newestwordpress}-blue.svg?style=flat-square) ![Compatible_with PHP_${newestphp}](https://img.shields.io/badge/Compatible_with-PHP_${newestphp}-green.svg?style=flat-square)
+
+This project is hand made for customer. Customer basic details are here:
+
+**Company name:** (Please fill)
+**Contact person:** (Please fill)
+**Contact email:** (Please fill)
+
+## Stack
+
+This project is built on [digitoimistodude/dudestack](https://github.com/digitoimistodude/dudestack), [digitoimistodude/air-light](https://github.com/digitoimistodude/air-light), [digitoimistodude/marlin-vagrant](https://github.com/digitoimistodude/marlin-vagrant) or [digitoimistodude/macos-lemp-setup](https://github.com/digitoimistodude/macos-lemp-setup), [digitoimistodude/devpackages](https://github.com/digitoimistodude/devpackages) which are documented at their corresponding locations. This is a guide on:
+
+- How to setup the project initially
+- How to update the project dependencies
+- Project settings and features
+
+## Basic details
+
+**Developers:** [ronilaukkarinen](https://github.com/ronilaukkarinen)
+**Custom post type plugin:** ACF Pro
+
+## Theme screenshot
+
+![Screenshot](/content/themes/${THEMENAME}/screenshot.png?raw=true \"Screenshot\")
+
+## Features
+
+On top of features included in [digitoimistodude/air-light](https://github.com/digitoimistodude/air-light), this project contains:
+
+- (Please fill)
+
+## Environments:
+
+Green checkmarks show if the environment is already set up and running, red cross indicates if it's not yet there or disabled.
+
+✅ Development: [${PROJECTNAME}.test](http://${PROJECTNAME}.test)
+❌ Staging: [asiakas.dude.fi/${PROJECTNAME}](https://asiakas.dude.fi/${PROJECTNAME})
+❌ Production: [${PROJECTNAME}.fi](https://${PROJECTNAME}.fi/)
+
+## Setting it up initially
+
+According to our handbook, you should have run `createproject`, inital setup by now. Your Vagrant machine (or macOS LEMP) should be up and running. If not, go back to [dudestack-instructions](https://github.com/digitoimistodude/dudestack-instructions) or [Dude handbook](https://handbook.dude.fi/wordpress-kehitys/projektin-aloitus) and read what you have do.
+
+If dev environment is indeed running, you're ready to version control the project.
+
+There are npm packages in both project root and theme folder. If you come later to this project, please run:
+
+1. `composer install` (in project folder)
+2. `npm install` (in project folder)
+2. `npm install` (in theme folder)
+
+Run project with `gulp`.
+
+## Noteworthy in this project
+
+Not at the moment." > "${HOME}/Projects/${PROJECTNAME}/README.md"
+
 #echo "${yellow}Activating theme...${txtreset}"
 #cd ${PROJECTPATH}
 #ssh vagrant@10.1.2.4 "cd /var/www/$PROJECTNAME/;vendor/wp-cli/wp-cli/bin/wp theme activate $THEMENAME"
