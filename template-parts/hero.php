61c17a1dee6275e31f830fd6537b5be37e92e120
Roni Laukkarinen <roni@dude.fi>
2019-10-11 15:46:02 +0300

    Change PHP-only to curly brackets format

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -9,18 +9,18 @@
  */
 
 // Block settings
-if ( is_front_page() ) :
+if ( is_front_page() ) {
 	$block_class = ' block-front';
-else :
+} else {
 	$block_class = ' block-' . get_post_type();
-endif;
+}
 
 // Featured image
-if ( has_post_thumbnail() ) :
+if ( has_post_thumbnail() ) {
 	$featured_image = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
-else :
+} else {
 	$featured_image = get_theme_file_uri( 'images/default.jpg' );
-endif;
+}
 ?>
 
 <div class="block<?php echo esc_attr( $block_class ); ?>" style="background-image: url('<?php echo esc_url( $featured_image ); ?>');">
