f892713a4a4c1f4437d3d753ed340ea4820f3d0a
Roni Laukkarinen <roni@dude.fi>
2019-10-15 13:43:08 +0300

    Remove leftover responsive-font

--- newtheme.sh
+++ newtheme.sh
@@ -106,7 +106,7 @@ chmod 777 ${PROJECTPATH}/media
 
 echo "${yellow}Generating default README.md...${txtreset}"
 
-newestair="4.8.3"
+newestair="4.8.4"
 newestwordpress="5.2.2"
 newestphp="7.2"
 currentdate=$(LC_TIME=en_US date '+%d %b %Y' |tr ' ' '_');
