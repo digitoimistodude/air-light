44af54eb7278a8310bd67d6caa01748b12d4bfe2
Roni Laukkarinen <roni@dude.fi>
2020-07-28 17:38:50 +0300

    Fix WordPress.WP.I18n.TextDomainMismatch

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@ namespace Air_Light;
 
   <?php get_template_part( 'template-parts/footer/demo-content' ); ?>
 
-  <p class="back-to-top"><a href="#page" class="js-trigger top no-text-link" data-mt-duration="300"><span class="screen-reader-text"><?php echo esc_html_e( 'Back to top', 'dude' ); ?></span><?php include get_theme_file_path( '/svg/chevron-up.svg' ); ?></a></p>
+  <p class="back-to-top"><a href="#page" class="js-trigger top no-text-link" data-mt-duration="300"><span class="screen-reader-text"><?php echo esc_html_e( 'Back to top', 'air-light' ); ?></span><?php include get_theme_file_path( '/svg/chevron-up.svg' ); ?></a></p>
 
 </footer><!-- #colophon -->
 
