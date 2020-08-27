1a12d5448b2f298c55c6dbd94bec94cc553d3fd6
Roni Laukkarinen <roni@dude.fi>
2020-08-27 10:53:21 +0300

    Change task order

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -7,7 +7,7 @@ const { handleError } = require('../helpers/handle-errors.js');
 // Task
 function watchfiles() {
   bs.init(config.browsersync.src, config.browsersync.opts);
-  watch(config.styles.src, series('styles', 'scsslint')).on('error', handleError('styles'));
+  watch(config.styles.src, series('scsslint', 'styles')).on('error', handleError('styles'));
   watch(config.php.src, series('phpcs')).on('change', bs.reload);
   watch(config.js.src, series('js')).on('change', bs.reload);
 };
