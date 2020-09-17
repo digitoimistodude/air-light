f92841a4a475c28f97e88ea44b0a3cc0cfba864b
Roni Laukkarinen <roni@dude.fi>
2020-09-17 14:09:01 +0300

    Fix packing script restore command

--- bin/air-pack.sh
+++ bin/air-pack.sh
@@ -9,4 +9,5 @@ rm ~/Projects/airdev/content/themes/air-light.zip
 sh ~/Projects/airdev/content/themes/air-light/bin/air-move-out.sh
 cd ~/Projects/airdev/content/themes/
 zip -r air-light.zip air-light
-sh ~/Projects/airdev/content/themes/air-light/bin/air-move-in.sh
+sh ~/air-temp/bin/air-move-in.sh
+
