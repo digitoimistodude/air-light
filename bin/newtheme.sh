5d59fb94a25fa7f7a236c01707997cac114f65e2
Roni Laukkarinen <roni@dude.fi>
2020-09-17 11:38:39 +0300

    Fix demo content nav alignment

--- bin/newtheme.sh
+++ bin/newtheme.sh
@@ -147,7 +147,7 @@ chmod 777 ${PROJECTPATH}/media
 
 echo "${yellow}Generating default README.md...${txtreset}"
 
-newestair="5.6.5"
+newestair="5.6.6"
 newestwordpress="5.5"
 newestphp="7.2"
 currentdate=$(LC_TIME=en_US date '+%d %b %Y' |tr ' ' '_');
--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ namespace Air_Light;
 /**
  * The current version of the theme.
  */
-define( 'AIR_LIGHT_VERSION', '5.6.5' );
+define( 'AIR_LIGHT_VERSION', '5.6.6' );
 
 /**
  * Theme settings
