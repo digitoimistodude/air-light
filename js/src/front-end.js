feb816fd6589a1691d10632e2577be16f30f46f6
Niku Hietanen <niku.hietanen@gmail.com>
2020-10-06 16:41:34 +0300

    Change root js to match refactoring

--- js/src/front-end.js
+++ js/src/front-end.js
@@ -3,14 +3,14 @@
  */
 
 // Import modules (comment to disable)
-import MoveTo from 'moveto';
+import BackToTop from './modules/BackToTop';
+import LazyLoad from './modules/LazyLoad';
 // import './modules/sticky-nav.js'
 // import slick from 'slick-carousel';
 import 'what-input';
-import './modules/lazyload.js';
 
 // Navigation
-import './modules/navigation.js';
+import './modules/navigation';
 
 // Define Javascript is active by changing the body class
 document.body.classList.remove('no-js');
@@ -19,126 +19,15 @@ document.body.classList.add('js');
 // Init lazyload
 // Usage example on template side when air-helper enabled:
 // <?php image_lazyload_tag( get_post_thumbnail_id( $post->ID ) ); ?>
-let images = document.querySelectorAll('.lazyload');
-lazyload(images, {
+
+const lazyload = new LazyLoad(document.querySelectorAll('.lazyload'), {
   root: null,
   rootMargin: '0px',
-  threshold: 0
+  threshold: 0,
 });
 
-// jQuery start
-(function ($) {
-  // Accessibility: Ensure back to top is right color on right background
-  // Note: Needs .has-light-bg or .has-dark-bg class on all blocks
-  var stickyOffset = $('.back-to-top').offset();
-  var $contentDivs = $('.block');
-  $(document).scroll(function () {
-    $contentDivs.each(function (k) {
-      var _thisOffset = $(this).offset();
-      var _actPosition = _thisOffset.top - $(window).scrollTop();
-      if (
-        _actPosition < stickyOffset.top &&
-        _actPosition + $(this).height() > 0
-      ) {
-        $('.back-to-top')
-          .removeClass('has-light-bg has-dark-bg')
-          .addClass(
-            $(this).hasClass('has-light-bg') ? 'has-light-bg' : 'has-dark-bg'
-          );
-        return false;
-      }
-    });
-  });
+lazyload.init();
 
-  // Detect Visible section on viewport from bottom
-  // @link https://codepen.io/BoyWithSilverWings/pen/MJgQqR
-  $.fn.isInViewport = function () {
-    var elementTop = $(this).offset().top;
-    var elementBottom = elementTop + $(this).outerHeight();
+const backToTop = new BackToTop();
 
-    var viewportTop = $(window).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
-
-    return elementBottom > viewportTop && elementTop < viewportBottom;
-  };
-
-  // Accessibility: Ensure back to top is right color on right background
-  $(window).on('resize scroll', function () {
-    $('.block').each(function () {
-      if ($(this).isInViewport()) {
-        $('.back-to-top')
-          .removeClass('has-light-bg has-dark-bg')
-          .addClass(
-            $(this).hasClass('has-light-bg') ? 'has-light-bg' : 'has-dark-bg'
-          );
-      }
-    });
-  });
-
-  // Accessibility add "External link:" aria label for external links
-  var currentHost = new RegExp(location.host);
-  $('a').each(function () {
-    var attr = $(this).attr('aria-label');
-    if (!currentHost.test($(this).attr('href')) && !attr) {
-      if ('#content' !== $(this).attr('href')) {
-        // A link that does not contain the current host
-        var txt = $(this).text();
-        $(this).addClass('is-external-link');
-        $(this).attr('aria-label', air_light_screenReaderText.external_link + ' ' + txt);
-      }
-    }
-
-    // If is outside link and has target="_blank"
-    if (!currentHost.test($(this).attr('href')) && !attr && '_blank' === $(this).attr('target')) {
-      $(this).attr('aria-label', maailma2030_screenReaderText.external_link + ', ' + maailma2030_screenReaderText.target_blank + ' ' + txt);
-    }
-  });
-
-  // Hide or show the 'back to top' link
-  $(window).scroll(function () {
-    // Back to top
-    var offset = 300; // Browser window scroll (in pixels) after which the 'back to top' link is shown
-    var offset_opacity = 1200; // Browser window scroll (in pixels) after which the link opacity is reduced
-    var scroll_top_duration = 700; // Duration of the top scrolling animation (in ms)
-    var link_class = '.back-to-top';
-
-    if ($(this).scrollTop() > offset) {
-      $(link_class).addClass('is-visible');
-    } else {
-      $(link_class).removeClass('is-visible');
-    }
-
-    if ($(this).scrollTop() > offset_opacity) {
-      $(link_class).addClass('fade-out');
-    } else {
-      $(link_class).removeClass('fade-out');
-    }
-  });
-
-  // Document ready start
-  $(function () {
-    // Your JavaScript here
-  });
-})(jQuery);
-
-document.addEventListener('DOMContentLoaded', function () {
-  const easeFunctions = {
-    easeInQuad: function (t, b, c, d) {
-      t /= d;
-      return c * t * t + b;
-    },
-    easeOutQuad: function (t, b, c, d) {
-      t /= d;
-      return -c * t * (t - 2) + b;
-    }
-  };
-  const moveTo = new MoveTo({
-      ease: 'easeInQuad'
-    },
-    easeFunctions
-  );
-  const triggers = document.getElementsByClassName('js-trigger');
-  for (var i = 0; i < triggers.length; i++) {
-    moveTo.registerTrigger(triggers[i]);
-  }
-});
+backToTop.init();
