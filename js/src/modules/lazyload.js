25e1710c2f676b2b487232395089d8d15fb266e4
Niku Hietanen <niku.hietanen@gmail.com>
2020-10-06 09:16:31 +0300

    Improve and refactor lazyload module

--- js/src/modules/lazyload.js
+++ js/src/modules/lazyload.js
@@ -14,203 +14,111 @@
  *
  */
 
-(function (root, factory) {
-  if (typeof exports === 'object') {
-    module.exports = factory(root);
-  } else if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else {
-    root.LazyLoad = factory(root);
-  }
-})(typeof global !== 'undefined' ? global : this.window || this.global, function (root) {
-
-  'use strict';
-
-  if (typeof define === 'function' && define.amd) {
-    root = window;
-  }
-
-  const defaults = {
-    src: 'data-src',
-    srcmobile: 'data-src-mobile',
-    srcset: 'data-srcset',
-    selector: '.lazyload',
-    root: null,
-    rootMargin: '0px',
-    threshold: 0
-  };
-
-  /**
-   * Merge two or more objects. Returns a new object.
-   * @private
-   * @param {Boolean}  deep     If true, do a deep (or recursive) merge [optional]
-   * @param {Object}   objects  The objects to merge together
-   * @returns {Object}          Merged values of defaults and options
-   */
-  const extend = function () {
-
-    let extended = {};
-    let deep = false;
-    let i = 0;
-    let length = arguments.length;
-
-    /* Check if a deep merge */
-    if (Object.prototype.toString.call(arguments[0]) === '[object Boolean]') {
-      deep = arguments[0];
-      i++;
-    }
-
-    /* Merge the object into the extended object */
-    let merge = function (obj) {
-      for (let prop in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, prop)) {
-          /* If deep merge and property is an object, merge properties */
-          if (deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
-            extended[prop] = extend(true, extended[prop], obj[prop]);
-          } else {
-            extended[prop] = obj[prop];
-          }
-        }
-      }
+export default class LazyLoad {
+  constructor(images, options) {
+    this.settings = {
+      src: 'data-src',
+      srcmobile: 'data-src-mobile',
+      srcset: 'data-srcset',
+      selector: '.lazyload',
+      root: null,
+      rootMargin: '0px',
+      threshold: 0,
+      breakpoint: 600,
+      ...options,
     };
-
-    /* Loop through each object and conduct a merge */
-    for (; i < length; i++) {
-      let obj = arguments[i];
-      merge(obj);
-    }
-
-    return extended;
-  };
-
-  function LazyLoad(images, options) {
-    this.settings = extend(defaults, options || {});
     this.images = images || document.querySelectorAll(this.settings.selector);
     this.observer = null;
-    this.init();
   }
 
-  LazyLoad.prototype = {
-    init: function () {
-
-      /* Without observers load everything and bail out early.
+  /**
+   * Initialize lazyload
+   */
+  init() {
+    /* Without observers load everything and bail out early.
          This affects some iOS and Windows Phones */
-      if (!root.IntersectionObserver) {
-        this.loadImages();
-        return;
-      }
-
-      let self = this;
-      let observerConfig = {
-        root: this.settings.root,
-        rootMargin: this.settings.rootMargin,
-        threshold: [this.settings.threshold]
-      };
-
-      this.observer = new IntersectionObserver(function (entries) {
-        Array.prototype.forEach.call(entries, function (entry) {
-
-          /* If inside viewport */
-          if (entry.isIntersecting) {
-
-            /* Define image */
-            const img = entry.target;
-
-            /* Add animation class to full-image div */
-            if (typeof (img.nextElementSibling) != 'undefined' && img.nextElementSibling != null) {
-              img.nextElementSibling.classList.add('reveal');
-            }
-
-            self.observer.unobserve(entry.target);
-            let src = img.getAttribute(self.settings.src);
-            let srcset = img.getAttribute(self.settings.srcset);
-            let srcmobile = img.getAttribute(self.settings.srcmobile);
-
-            /* Replace fully loaded original background image to the img src */
-            if ('img' === img.tagName.toLowerCase()) {
-              if (document.documentElement.clientWidth < 600) {
-                img.src = srcmobile;
-              } else {
-                img.src = src;
-              }
-            } else {
-              /* Add fully loaded original background image to next div element */
-              if (document.documentElement.clientWidth < 600) {
-                img.nextElementSibling.style.backgroundImage = 'url(' + srcmobile + ')';
-              } else {
-                img.nextElementSibling.style.backgroundImage = 'url(' + src + ')';
-              }
-            }
-          }
-        });
-      }, observerConfig);
-
-      Array.prototype.forEach.call(this.images, function (image) {
-        self.observer.observe(image);
-      });
-    },
-
-    loadAndDestroy: function () {
-      if (!this.settings) {
-        return;
-      }
+    if (!window.IntersectionObserver) {
       this.loadImages();
-      this.destroy();
-    },
+      return;
+    }
 
-    loadImages: function () {
-      if (!this.settings) {
-        return;
-      }
+    const self = this;
+    const observerConfig = {
+      root: this.settings.root,
+      rootMargin: this.settings.rootMargin,
+      threshold: [this.settings.threshold],
+    };
 
-      let self = this;
-      Array.prototype.forEach.call(this.images, function (image) {
-        let src = image.getAttribute(self.settings.src);
-        let srcset = image.getAttribute(self.settings.srcset);
-        let srcmobile = image.getAttribute(self.settings.srcmobile);
+    this.observer = new IntersectionObserver(((entries) => {
+      Array.prototype.forEach.call(entries, (entry) => {
+        /* If inside viewport */
+        if (entry.isIntersecting) {
+          /* Define image */
+          const img = entry.target;
 
-        if ('img' === image.tagName.toLowerCase()) {
-          if (src) {
-            image.src = src;
+          /* Add animation class to full-image div */
+          if (typeof (img.nextElementSibling) !== 'undefined' && img.nextElementSibling != null) {
+            img.nextElementSibling.classList.add('reveal');
           }
-          if (srcset) {
-            image.srcset = srcset;
-          }
-          if (srcmobile) {
-            image.srcmobile = srcmobile;
-          }
-        } else {
-          if (document.documentElement.clientWidth < 600) {
-            image.style.backgroundImage = 'url("' + srcmobile + '")';
+
+          self.observer.unobserve(entry.target);
+          const src = img.getAttribute(self.settings.src);
+          const srcset = img.getAttribute(self.settings.srcset);
+          const srcmobile = img.getAttribute(self.settings.srcmobile);
+
+          /* Replace fully loaded original background image to the img src */
+          if (img.tagName.toLowerCase() === 'img') {
+            img.src = document.documentElement.clientWidth < this.settings.breakpoint
+              ? srcmobile : src;
           } else {
-            image.style.backgroundImage = 'url("' + src + '")';
+            // Add fully loaded original background image to next div element
+            img.nextElementSibling.style.backgroundImage = document.documentElement.clientWidth < this.settings.breakpoint ? `url(${srcmobile})` : `url(${src})`;
           }
         }
       });
-    },
+    }), observerConfig);
 
-    destroy: function () {
-      if (!this.settings) {
-        return;
-      }
-      this.observer.disconnect();
-      this.settings = null;
-    }
-  };
-
-  root.lazyload = function (images, options) {
-    return new LazyLoad(images, options);
-  };
-
-  if (root.jQuery) {
-    const $ = root.jQuery;
-    $.fn.lazyload = function (options) {
-      options = options || {};
-      options.attribute = options.attribute || 'data-src';
-      new LazyLoad($.makeArray(this), options);
-      return this;
-    };
+    Array.prototype.forEach.call(this.images, (image) => {
+      self.observer.observe(image);
+    });
   }
 
-  return LazyLoad;
-});
+  loadAndDestroy() {
+    if (!this.settings) {
+      return;
+    }
+    this.loadImages();
+    this.destroy();
+  }
+
+  /**
+   * Loads all images at once, used for fallbacks on some mobile browsers
+   */
+  loadImages() {
+    if (!this.settings) {
+      return;
+    }
+
+    this.images.forEach((image) => {
+      const src = image.getAttribute(this.settings.src);
+      const srcset = image.getAttribute(this.settings.srcset);
+      const srcmobile = image.getAttribute(this.settings.srcmobile);
+
+      if (image.tagName.toLowerCase() === 'img') {
+        image.src = src !== image.src;
+        image.srcset = srcset !== image.srcset;
+        image.srcmobile = srcmobile !== image.srcmobile;
+      } else {
+        image.style.backgroundImage = document.documentElement.clientWidth < this.settings.breakpoint ? `url("${srcmobile}")` : `url("${src}")`;
+      }
+    });
+  }
+
+  destroy() {
+    if (!this.settings) {
+      return;
+    }
+    this.observer.disconnect();
+    this.settings = null;
+  }
+}
