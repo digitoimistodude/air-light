f4a7827eda009521252efbff17a0fd489cd41295
Niku Hietanen <niku.hietanen@gmail.com>
2020-10-06 09:21:04 +0300

    Add a few exceptions to lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   parser: 'babel-eslint',
   extends: 'eslint-config-airbnb/base',
   rules: {
-    indent: ['error', 2]
+    indent: ['error', 2],
+    'func-names': 0,
+    'no-param-reassign': ['error', { props: false }],
   },
   env: {
     browser: true,
-    jquery: true
-  }
+    jquery: true,
+  },
 };
