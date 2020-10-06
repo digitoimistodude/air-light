38e477dc11368c0b336e81a896ef8031c5b66aaa
Niku Hietanen <niku.hietanen@gmail.com>
2020-10-06 16:40:31 +0300

    Add debounce helper

---
+++ js/src/modules/helpers.js
@@ -0,0 +1,19 @@
+/**
+ * A typical debounce
+ */
+/* eslint-disable-next-line import/prefer-default-export */
+export default function debounce(func, wait, immediate) {
+  let timeout;
+  return function () {
+    const context = this; const
+      args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
