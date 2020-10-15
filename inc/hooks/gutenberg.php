a61ba5cda5757b1c1c9eff5668fe6a13626888e1
Roni Laukkarinen <roni@dude.fi>
2020-10-15 14:43:17 +0300

    Register Gutenberg wp-admin editor style, simplify defaults, 6.1.4

--- inc/hooks/gutenberg.php
+++ inc/hooks/gutenberg.php
@@ -60,3 +60,15 @@ function register_block_editor_assets() {
   wp_register_style( 'block-editor', get_theme_file_uri( 'css/gutenberg.min.css', __FILE__ ), null, null ); // phpcs:ignore
 }
 add_action( 'admin_init', __NAMESPACE__ . '\register_block_editor_assets' );
+
+/**
+ * Register Gutenberg wp-admin editor style
+ */
+function setup_editor_styles() {
+  // Add support for editor styles.
+  add_theme_support( 'editor-styles' );
+
+  // Enqueue editor styles.
+  add_editor_style( get_theme_file_uri( 'css/gutenberg.min.css' ) );
+}
+add_action( 'after_setup_theme', __NAMESPACE__ . '\setup_editor_styles' );
