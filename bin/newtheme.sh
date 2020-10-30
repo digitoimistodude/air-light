204a27ed5bf7215b772d013a8636c0182a6fee98
Roni Laukkarinen <roni@dude.fi>
2020-10-30 13:47:00 +0200

    Ensure line breaks in default README.md for project

--- bin/newtheme.sh
+++ bin/newtheme.sh
@@ -163,42 +163,26 @@ currentdate=$(LC_TIME=en_US date '+%d %b %Y' |tr ' ' '_');
 echo "# ${PROJECTNAME}
 ![based_on_air_version ${newestair}_](https://img.shields.io/badge/based_on_air_version-${newestair}_-brightgreen.svg?style=flat-square) ![project_created ${currentdate}](https://img.shields.io/badge/project_created-${currentdate}-blue.svg?style=flat-square) ![Tested_up_to WordPress_${newestwordpress}](https://img.shields.io/badge/Tested_up_to-WordPress_${newestwordpress}-blue.svg?style=flat-square) ![Compatible_with PHP_${newestphp}](https://img.shields.io/badge/Compatible_with-PHP_${newestphp}-green.svg?style=flat-square)
 
-This project is hand made for customer. Customer basic details are here:
-
-**Company name:** (Please fill)
-**Contact person:** (Please fill)
-**Contact email:** (Please fill)
-
+This project is hand made for customer.
 ## Stack
 
 This project is built on [digitoimistodude/dudestack](https://github.com/digitoimistodude/dudestack), [digitoimistodude/air-light](https://github.com/digitoimistodude/air-light), [digitoimistodude/marlin-vagrant](https://github.com/digitoimistodude/marlin-vagrant) or [digitoimistodude/macos-lemp-setup](https://github.com/digitoimistodude/macos-lemp-setup), [digitoimistodude/devpackages](https://github.com/digitoimistodude/devpackages) which are documented at their corresponding locations. This is a guide on:
 
-- How to setup the project initially
-- How to update the project dependencies
+- How to setup the project initially<br>
+- How to update the project dependencies<br>
 - Project settings and features
-
-## Basic details
-
-**Developers:** [ronilaukkarinen](https://github.com/ronilaukkarinen)
-**Custom post type plugin:** ACF Pro
 
 ## Theme screenshot
 
 ![Screenshot](/content/themes/${THEMENAME}/screenshot.png?raw=true \"Screenshot\")
 
-## Features
-
-On top of features included in [digitoimistodude/air-light](https://github.com/digitoimistodude/air-light), this project contains:
-
-- (Please fill)
-
 ## Environments:
 
 Green checkmarks show if the environment is already set up and running, red cross indicates if it's not yet there or disabled.
 
-✅ Development: [${PROJECTNAME}.test](http://${PROJECTNAME}.test)
-❌ Staging: [${PROJECTNAME}.vaiheessa.fi](https://${PROJECTNAME}.vaiheessa.fi)
-❌ Production: [${PROJECTNAME}.fi](https://${PROJECTNAME}.fi/)
+✅  Development: [${PROJECTNAME}.test](http://${PROJECTNAME}.test)<br>
+❌  Staging: [${PROJECTNAME}.vaiheessa.fi](https://${PROJECTNAME}.vaiheessa.fi)<br>
+❌  Production: [${PROJECTNAME}.fi](https://${PROJECTNAME}.fi/)
 
 ## Setting it up initially
 
@@ -212,11 +196,7 @@ There are npm packages in both project root and theme folder. If you come later 
 2. \`npm install\` (in project folder)
 2. \`npm install\` (in theme folder)
 
-Run project with \`gulp\`.
-
-## Noteworthy in this project
-
-Not at the moment." > "${HOME}/Projects/${PROJECTNAME}/README.md"
+Run project with \`gulp\`." > "${HOME}/Projects/${PROJECTNAME}/README.md"
 
 #echo "${yellow}Activating theme...${txtreset}"
 #cd ${PROJECTPATH}
