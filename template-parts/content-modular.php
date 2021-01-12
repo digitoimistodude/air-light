581dac679838b592368957b64aa96d20dafc85a7
Timi Wahalahti <timi@wahalahti.fi>
2021-01-12 15:59:36 +0200

    simplify modular content partial

--- template-parts/content-modular.php
+++ template-parts/content-modular.php
@@ -4,8 +4,8 @@
  *
  * @Author: Niku Hietanen
  * @Date: 2020-02-28 15:38:00
- * @Last Modified by: Niku Hietanen
- * @Last Modified time: 2020-02-28 16:34:01
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2021-01-12 15:59:01
  * @package air-light
  */
 
@@ -14,15 +14,10 @@ namespace Air_Light;
 // Get the page id (can be something else than the page id)
 $have_rows_id = get_modular_rows_id();
 
-// check if there is modules.
-if ( function_exists( 'have_rows' ) && \have_rows( 'modular_content', $have_rows_id ) ) :
-
-  // loop modules.
-  while ( \have_rows( 'modular_content', $have_rows_id ) ) : \the_row();
-
+if ( function_exists( 'have_rows' ) && \have_rows( 'modular_content', $have_rows_id ) ) {
+  while ( \have_rows( 'modular_content', $have_rows_id ) ) {
+    \the_row();
     the_module( $have_rows_id );
-
-  endwhile;
-
-endif;
+  }
+}
 
