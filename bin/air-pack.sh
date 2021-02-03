243450f7a7d0f28b8597b4f5163b657ed7213144
Roni Laukkarinen <roni@dude.fi>
2021-02-03 14:21:42 +0200

    Add color variables

--- bin/air-pack.sh
+++ bin/air-pack.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # A script that makes a package for WordPress Theme Directory
+txtbold=$(tput bold)
+boldyellow=${txtbold}$(tput setaf 3)
+boldgreen=${txtbold}$(tput setaf 2)
+boldwhite=${txtbold}$(tput setaf 7)
+yellow=$(tput setaf 3)
+green=$(tput setaf 2)
+white=$(tput setaf 7)
+txtreset=$(tput sgr0)
 
 mkdir -p ~/Projects
 mkdir -p ~/Projects/airdev
