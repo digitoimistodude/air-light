c2667fbd3fa94f0805e7e49b0bf162b795203d47
Roni Laukkarinen <roni@dude.fi>
2021-02-19 16:31:52 +0200

    Remove leftover bundle

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,5 @@ exports.js = require('./gulp/tasks/js.js').js;
 exports.phpcs = require('./gulp/tasks/phpcs.js').phpcs;
 exports.scsslint = require('./gulp/tasks/scsslint.js').scsslint;
 exports.styles = require('./gulp/tasks/styles.js').styles;
-exports.gutenbergstyles = require('./gulp/tasks/styles.js').gutenbergstyles;
 exports.watch = require('./gulp/tasks/watch.js').watch;
 exports.default = require('./gulp/tasks/watch.js').watch;
