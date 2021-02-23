47757408089512d46cb2d6bf68daa3c68b82a420
Timi Wahalahti <timi@wahalahti.fi>
2021-02-23 13:44:05 +0200

    no default image in theme

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -7,18 +7,22 @@
  *
  * @Date:   2019-10-15 12:30:02
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2021-01-12 16:00:20
+ * @Last Modified time: 2021-02-23 13:43:29
  * @package air-light
  */
 
 // Block settings
-$block_class = is_front_page() ? ' block-hero-front' : ' block-hero-' . get_post_type();
+$block_classes[] = is_front_page() ? ' block-hero-front' : ' block-hero-' . get_post_type();
 
 // Featured image
 $featured_image = has_post_thumbnail() ? wp_get_attachment_url( get_post_thumbnail_id() ) : THEME_SETTINGS['default_featured_image'];
 ?>
 
-<section class="block block-hero<?php echo esc_attr( $block_class ); ?>" style="background-image: url('<?php echo esc_url( $featured_image ); ?>');">
+<section class="block block-hero<?php echo esc_attr( implode( ' ', $block_classes ) ); ?>"
+  <?php if ( ! empty( $featured_image ) ) : ?>
+    style="background-image: url('<?php echo esc_url( $featured_image ); ?>');"
+  <?php endif; ?>
+>
   <div class="shade" aria-hidden="true"></div>
 
   <!-- <div class="container">
