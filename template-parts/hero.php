a8c5534e37776c0033bbad38fb7558544453a870
Timi Wahalahti <timi@wahalahti.fi>
2021-02-23 13:59:54 +0200

    fix header classes

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -7,7 +7,7 @@
  *
  * @Date:   2019-10-15 12:30:02
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2021-02-23 13:43:29
+ * @Last Modified time: 2021-02-23 13:59:45
  * @package air-light
  */
 
@@ -18,7 +18,7 @@ $block_classes[] = is_front_page() ? ' block-hero-front' : ' block-hero-' . get_
 $featured_image = has_post_thumbnail() ? wp_get_attachment_url( get_post_thumbnail_id() ) : THEME_SETTINGS['default_featured_image'];
 ?>
 
-<section class="block block-hero<?php echo esc_attr( implode( ' ', $block_classes ) ); ?>"
+<section class="block block-hero <?php echo esc_attr( implode( ' ', $block_classes ) ); ?>"
   <?php if ( ! empty( $featured_image ) ) : ?>
     style="background-image: url('<?php echo esc_url( $featured_image ); ?>');"
   <?php endif; ?>
