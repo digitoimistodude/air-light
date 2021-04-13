c9ec47022036b83e26177ec8dc682aefe2468f88
Roni Laukkarinen <roni@dude.fi>
2021-04-13 20:20:10 +0300

    Fix order

--- bin/tasks/replaces-wsl.sh
+++ bin/tasks/replaces-wsl.sh
@@ -1,6 +1,7 @@
 echo "${YELLOW}Generating theme files with theme name and textdomain called ${THEME_NAME}${TXTRESET}"
 # THE magical sed command by rolle (goes through every single file in theme folder and searchs and replaces every air instance with THEME_NAME):
 # WSL/Ubuntu version of sed binary, different format than on macOS
+# Note: find + -exec sed doesn't work in WSL for some weird reason so we have to use "s;string;replacewith;" format
 for i in `grep -rl air-light * 2> /dev/null`; do LC_ALL=C sed -i -e "s;air-light;${THEME_NAME};" $i; done
 for i in `grep -rl Air-light * 2> /dev/null`; do LC_ALL=C sed -i -e "s;Air-light;${THEME_NAME};" $i; done
 for i in `grep -rl air * 2> /dev/null`; do LC_ALL=C sed -i -e "s;air-light;${THEME_NAME};" $i; done
@@ -9,9 +10,6 @@ for i in `grep -rl air * 2> /dev/null`; do LC_ALL=C sed -i -e "s;Air_light_;${TH
 
 # Remove demo content
 echo "${YELLOW}Removing demo content...${TXTRESET}"
-for i in `grep -rl air * 2> /dev/null`; do LC_ALL=C sed -i -e "s;Air_light_;${THEME_NAME}_;" $i; done
-
-# Note: find + -exec sed doesn't work in WSL for some weird reason
 LC_ALL=C sed -i -e "s;@import 'layout\/demo-content'\;;;" ${PROJECT_THEME_PATH}/sass/global.scss
 LC_ALL=C sed -i -e "s;@import 'layout\/wordpress'\;;;" ${PROJECT_THEME_PATH}/sass/global.scss
 LC_ALL=C sed -i -e "s;<\?php get_template_part( \'template-parts\/header\/demo-content\' ); \?>;;" ${PROJECT_THEME_PATH}/front-page.php
