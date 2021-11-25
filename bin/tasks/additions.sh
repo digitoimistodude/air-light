83a5b7910fd635a9d2aee3e35aa5d971cc84f362
Roni Laukkarinen <roni@dude.fi>
2021-11-25 13:00:44 +0200

    Fix escapes

--- bin/tasks/additions.sh
+++ bin/tasks/additions.sh
@@ -2,7 +2,7 @@
 # @Author: Roni Laukkarinen
 # @Date:   2021-05-21 14:40:29
 # @Last Modified by:   Roni Laukkarinen
-# @Last Modified time: 2021-11-24 15:19:05
+# @Last Modified time: 2021-11-25 12:59:57
 echo "${YELLOW}Adding media library folder...${TXTRESET}"
 mkdir -p ${PROJECT_PATH}/media
 echo "" > ${PROJECT_PATH}/media/index.php
@@ -20,7 +20,7 @@ echo "# ${PROJECT_NAME}
 This project is hand made for customer by Dude.
 
 ------8<----------<br>
-**Disclaimer:** Please remove this disclaimer after you have edited the README.md, style.css version information and details and screenshot.png. If you see this text in place after the project has been deployed to production, `git blame` is in place ;)<br>
+**Disclaimer:** Please remove this disclaimer after you have edited the README.md, style.css version information and details and screenshot.png. If you see this text in place after the project has been deployed to production, \`git blame\` is in place ;)<br>
 ------8<----------
 
 ## Stack
