fca737e3cecf3dd75ef95898766762de4cae0666
Dylan Elliott <dylanelliott27@hotmail.com>
2022-01-12 10:24:11 -0500

    update deprecated filters and their parameters

--- inc/hooks.php
+++ inc/hooks.php
@@ -39,10 +39,10 @@ add_action( 'wp_default_scripts', __NAMESPACE__ . '\move_jquery_into_footer' );
  * Gutenberg associated hooks
  */
 require get_theme_file_path( 'inc/hooks/gutenberg.php' );
-add_filter( 'allowed_block_types', __NAMESPACE__ . '\allowed_block_types', 10, 2 );
+add_filter( 'allowed_block_types_all', __NAMESPACE__ . '\allowed_block_types', 10, 2 );
 add_filter( 'use_block_editor_for_post_type', __NAMESPACE__ . '\use_block_editor_for_post_type', 10, 2 );
 add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\register_block_editor_assets' );
-add_filter( 'block_editor_settings', __NAMESPACE__ . '\remove_gutenberg_inline_styles', 10, 2 );
+add_filter( 'block_editor_settings_all', __NAMESPACE__ . '\remove_gutenberg_inline_styles', 10, 2 );
 add_action( 'after_setup_theme', __NAMESPACE__ . '\setup_editor_styles' );
 add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\block_editor_title_input_styles' );
 
@@ -50,7 +50,7 @@ add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\block_editor_title_
  * ACF blocks
  */
 require get_theme_file_path( 'inc/hooks/acf-blocks.php' );
-add_filter( 'block_categories', __NAMESPACE__ . '\acf_blocks_add_category_in_gutenberg', 10, 2 );
+add_filter( 'block_categories_all', __NAMESPACE__ . '\acf_blocks_add_category_in_gutenberg', 10, 2 );
 add_action( 'acf/init', __NAMESPACE__ . '\acf_blocks_init' );
 
 /**
