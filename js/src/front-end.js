4da5c2ce6879168a2e4d12f1133d7ace221d779d
Roni Laukkarinen <roni@dude.fi>
2022-05-24 11:30:59 +0300

    A bit clenaer eslint-disable

--- js/src/front-end.js
+++ js/src/front-end.js
@@ -1,11 +1,10 @@
-/* eslint-disable max-len, no-underscore-dangle, no-param-reassign */
+/* eslint-disable max-len, no-underscore-dangle, no-param-reassign, no-unused-vars */
 /**
  * Air theme JavaScript.
  */
 
 // Import modules
 import reframe from 'reframe.js';
-// eslint-disable-next-line no-unused-vars
 import getLocalization from './modules/localization';
 import { styleExternalLinks, getChildAltText, initExternalLinkLabels } from './modules/external-link';
 import initAnchors from './modules/anchors';
