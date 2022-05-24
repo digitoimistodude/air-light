b86ecd02a26bacd4bca09dd022ea5fd648b0bde1
Roni Laukkarinen <roni@dude.fi>
2022-05-24 11:31:31 +0300

    Remove unused rule

--- js/src/front-end.js
+++ js/src/front-end.js
@@ -1,4 +1,4 @@
-/* eslint-disable max-len, no-underscore-dangle, no-param-reassign, no-unused-vars */
+/* eslint-disable max-len, no-param-reassign, no-unused-vars */
 /**
  * Air theme JavaScript.
  */
