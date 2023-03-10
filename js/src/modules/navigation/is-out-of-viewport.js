f86be34e3ca66dc716da6878e51375273db7c2ea
Michael Bourne <michaelbourne@users.noreply.github.com>
2023-03-10 13:59:35 -0700

    Fix navigation issues

--- js/src/modules/navigation/is-out-of-viewport.js
+++ js/src/modules/navigation/is-out-of-viewport.js
@@ -1,8 +1,8 @@
 /**
  * @Author: Roni Laukkarinen
  * @Date:   2022-12-31 00:19:27
- * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2022-12-31 00:19:32
+ * @Last Modified by:   Michael Bourne
+ * @Last Modified time: 2023-03-09 12:19:32
  */
 // Check if an element is out of the viewport
 // eslint-disable-next-line func-names
@@ -14,8 +14,8 @@ const isOutOfViewport = function (elem) {
   const out = {};
   out.top = bounding.top < 0;
   out.left = bounding.left < 0;
-  out.bottom = bounding.bottom > (window.innerHeight || document.documentElement.clientHeight);
-  out.right = bounding.right > (window.innerWidth || document.documentElement.clientWidth);
+  out.bottom = bounding.bottom >= (document.documentElement.clientHeight || document.body.clientHeight);
+  out.right = bounding.right >= (document.documentElement.clientWidth || document.body.clientWidth);
   out.any = out.top || out.left || out.bottom || out.right;
 
   return out;
