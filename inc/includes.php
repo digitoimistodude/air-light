02b12d6f29cd61e5fb8cafedf3110842a88d82b3
Roni Laukkarinen <roni@dude.fi>
2025-02-26 18:11:25 +0200

    Add nav walker for simplified footer navs, T-25859

--- inc/includes.php
+++ inc/includes.php
@@ -13,9 +13,9 @@ require get_theme_file_path( '/inc/includes/theme-setup.php' );
 // Localized strings
 require get_theme_file_path( '/inc/includes/localization.php' );
 
-// Nav Walker
+// Nav walkers
 require get_theme_file_path( '/inc/includes/nav-walker.php' );
-
+require get_theme_file_path( '/inc/includes/nav-walker-footer.php' );
 // Post type and taxonomy base classes
 // We check this with if, because this stuff will not go to WP theme directory
 if ( file_exists( get_theme_file_path( '/inc/includes/taxonomy.php' ) ) ) {
@@ -25,5 +25,3 @@ if ( file_exists( get_theme_file_path( '/inc/includes/taxonomy.php' ) ) ) {
 if ( file_exists( get_theme_file_path( '/inc/includes/post-type.php' ) ) ) {
   require get_theme_file_path( '/inc/includes/post-type.php' );
 }
-
-// Custom functions
