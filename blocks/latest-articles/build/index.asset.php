a9b7812ce1e4ef06759a3f9ae39e0acb1bdc24b1
Roni Laukkarinen <roni@dude.fi>
2025-03-07 14:03:41 +0200

    Prevent accidentally clicking the links, T-25910

--- blocks/latest-articles/build/index.asset.php
+++ blocks/latest-articles/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '6c2f7865cc4dc2556c39');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-i18n'), 'version' => '62083ba8da7689bbf491');
