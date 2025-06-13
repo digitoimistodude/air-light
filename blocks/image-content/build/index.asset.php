37723c31ad10001855ed5167b1e765543ccfbf21
Roni Laukkarinen <roni@dude.fi>
2025-06-13 12:36:32 +0300

    Finalize block styles, move font definitions to helpers, Ref: DEV-308, DEV-27

--- blocks/image-content/build/index.asset.php
+++ blocks/image-content/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5aea024f87463ffdf95e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd729e48155cc46409407');
