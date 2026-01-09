4b9420db5ff589e3fcb79baea9f833b39028b35b
Roni Laukkarinen <roni@dude.fi>
2026-01-09 14:57:36 +0200

    Fix embedded PHP formatting via PHPCBF, Ref: DEV-672

--- comments.php
+++ comments.php
@@ -27,7 +27,8 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area">
 
   <?php // You can start editing here -- including this comment!
-  if ( have_comments() ) : ?>
+  if ( have_comments() ) :
+  ?>
     <h2 class="comments-title">
       <?php $comment_count = get_comments_number();
       if ( '1' === $comment_count ) {
@@ -85,7 +86,8 @@ if ( post_password_required() ) {
 
 
   // If comments are closed and there are comments, let's leave a little note, shall we?
-  if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+  if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+  ?>
     <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'air-light' ); ?></p>
   <?php endif;
 
